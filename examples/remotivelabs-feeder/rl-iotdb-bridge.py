2bb15e0bd64b9ea39408c24e6a73c3ed6867a4fa
Stephen Lawrence <stephen.lawrence@renesas.com>
2024-05-03 19:26:46 +0100

    remotivelabs example: implement simple Apache IoTDB bridge
    Reimplement the simple bridge created for the Spring 2024 COVESA AMM.
    
    The code is simpler if we do not need to query IoTDB to see what
    timeseries schemas are in use. IoTDB can infer the data type from string
    data for defined schemas. So use this ability to achieve the simplicity
    goal. This also makes the example more readable.
    
    The bridge waits for the subscribed signals to be sent from the
    RemotiveLabs broker, formats them and then writes them to IoTDB using
    the specified IoTDB path.
    
    The code assumes the timeseries already exists.
    
    Signed-off-by: Stephen Lawrence <stephen.lawrence@renesas.com>

--- examples/remotivelabs-feeder/rl-iotdb-bridge.py
+++ examples/remotivelabs-feeder/rl-iotdb-bridge.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+
+# Simple bridge between RemotiveLabs cloud virtual signal platform and the
+# Apache IoTDB data store used in the Playground. The bridge subscribes to
+# the specified signals in the cloud and writes received frames into the
+# IoTDB store.
+#
+# Copyright (C) 2024 Renesas Electronics Corporation
+#           (C) 2024 RemotiveLabs
+#
+# Attribution: The bridge is largely based on the 'python subscribe' sample
+# from the RemotiveLabs sample repository [1].
+#
+# [1] https://github.com/remotivelabs/remotivelabs-samples/tree/1939efca4ce5e10a7001595bb4cbe81de4a9a74e/python/subscribe
+
 from __future__ import annotations
 
 import argparse
@@ -9,14 +24,55 @@ from remotivelabs.broker.sync import (
     BrokerException,
     SignalIdentifier,
 )
+from iotdb.Session import Session
 
+
+# Simple Apache IoTDB session configuration
+ip = "127.0.0.1"
+port_ = "6667"
+username_ = "root"
+password_ = "root"
+session = Session(ip, port_, username_, password_, fetch_size=1024, zone_id="GMT+00:00")
+device_id_ = "root.test2.dev1"
+
+def iotdb_open_session():
+    session.open(False)
+
+def iotdb_close_session():
+    session.close()
 
 def run_subscribe_sample(url: str, signals: list[str], secret: Optional[str] = None):
     client = Client(client_id="Sample client")
     client.connect(url=url, api_key=secret)
 
+    iotdb_open_session()
+
     def on_signals(signals_in_frame: SignalsInFrame):
         for signal in signals_in_frame:
+            # Convert RemotiveLabs timestamp in microseconds to milliseconds
+            ts_ = int(signal.timestamp_us()/1000)
+
+            # Support a wide range of possible signals without needing knowledge
+            # of the DB schema by converting data to string and asking IoTDB
+            # to infer the type based on the timeseries schema.
+
+            vss_name_ = signal.name()
+            if (
+                vss_name_ == 'Vehicle.Chassis.Accelerator.PedalPosition' or 
+                vss_name_ == 'Vehicle.Powertrain.Transmission.CurrentGear' or 
+                vss_name_ == 'Vehicle.Powertrain.TractionBattery.NominalVoltage' or 
+                vss_name_ == 'Vehicle.Chassis.SteeringWheel.Angle'
+            ):
+                # RemotiveLabs sends higher precision data types. Convert them to
+                # the int type expected for these VSS leaf nodes.
+                vss_value_ = [str(int(signal.value()))]
+            else:
+                vss_value_ = [str(signal.value())]
+
+            # Quote the VSS leaf node name
+            iotdb_vss_name_ = ['`{}`'.format(vss_name_)]
+            # IoTDB does data type inference for basic types based on the timeseries schema
+            session.insert_str_record(device_id_, ts_, iotdb_vss_name_, vss_value_)
             print(signal.to_json())
 
     client.on_signals = on_signals
@@ -36,9 +92,11 @@ def run_subscribe_sample(url: str, signals: list[str], secret: Optional[str] = N
         )
     except BrokerException as e:
         print(e)
+        iotdb_close_session()
         exit(1)
     except Exception as e:
         print(e)
+        iotdb_close_session()
         exit(1)
 
     try:
@@ -50,6 +108,7 @@ def run_subscribe_sample(url: str, signals: list[str], secret: Optional[str] = N
     except KeyboardInterrupt:
         subscription.cancel()
         print("Keyboard interrupt received, closing")
+        iotdb_close_session()
 
 
 def main():
